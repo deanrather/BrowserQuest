09e0774a697586ce1301a8cc6af28ef44c075129
Dean Rather <dearnather@gmail.com>
2015-06-29 08:27:44 +1000

    not really anything changed. just the line endings back to unix. maybe the modified stamp changed...

--- client/js/game.js
+++ client/js/game.js
@@ -1,4 +1,5 @@
 
+
 define(['infomanager', 'bubble', 'renderer', 'map', 'animation', 'sprite',
         'tile', 'warrior', 'gameclient', 'audio', 'updater', 'transition',
         'pathfinder', 'item', 'mob', 'npc', 'player', 'character', 'chest',
